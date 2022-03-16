06ec2620b86c9975fe8f1d4bc01ce6bb36993b66
vjrj <vjrj@ourproject.org>
2022-03-16 20:49:30 +0100

    Remove liquibase (#85)
    * Revert "Remove unnecessary collection_command migration steps (#83)"
    
    This reverts commit 513eb7c8626e9c7672d9e6c5f6b191b71c09e1a9.
    
    * Revert "Migration db to 3.X using data-migration/liquibase plugin (#81)"
    
    This reverts commit 72f538800a9d5349f505541b58f02bd51bc28359.

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,5 +1,2 @@
 // Added by the Audit-Logging plugin:
 grails.plugin.auditLog.auditDomainClassName = 'au.org.ala.collectory.AuditLogEvent'
-grails.plugin.databasemigration.changelogFileName = 'changelog.xml'
-grails.plugin.databasemigration.updateOnStart = true
-grails.plugin.databasemigration.updateOnStartFileName = 'changelog.xml'
