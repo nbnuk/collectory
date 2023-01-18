18c90e8c9b06d036f1d5f14a64349eda641a89e8
Sushant <105408469+sughics@users.noreply.github.com>
2023-01-18 15:33:22 +1100

    issue #175 updaing unit test for edit and admin intercetptors

--- src/test/groovy/au/org/ala/collectory/EditRoleInterceptorSpec.groovy
+++ src/test/groovy/au/org/ala/collectory/EditRoleInterceptorSpec.groovy
@@ -12,11 +12,36 @@ class EditRoleInterceptorSpec extends Specification implements InterceptorUnitTe
 
     }
 
-    void "Test role interceptor matching"() {
+    void "Test EditRole interceptor matching"() {
         when:"A request matches the interceptor"
-            withRequest(controller:"role")
+        withRequest(controller: 'collection', action: "list")
+        withRequest('controller': 'collection')
+        withRequest('controller': 'institution', action: 'list')
+        withRequest('controller': 'contact')
+        withRequest('controller': 'licence', action: 'create')
+        withRequest('controller': 'licence', action: 'edit')
+        withRequest('controller': 'licence', action: 'show')
+        withRequest('controller': 'licence', action: 'save')
+        withRequest('controller':'providerGroup')
+        withRequest('controller':'providerMap')
+        withRequest('controller':'providerCode')
+        withRequest('controller':'dataProvider')
+        withRequest('controller':'dataHub')
+        withRequest('controller':'reports')
 
         then:"The interceptor does match"
-            interceptor.doesMatch()
+        interceptor.doesMatch()
+    }
+
+
+    void "Test EditRole interceptor not matching"() {
+        when:"A request matches the interceptor"
+        withRequest(controller: 'data', action: "getEntity")
+        withRequest(controller: 'data', action: "mapToCsv")
+        withRequest(controller: 'admin')
+        withRequest(controller: 'randomController', action: "randomAction")
+
+        then:"The interceptor does not match"
+        !interceptor.doesMatch()
     }
 }
