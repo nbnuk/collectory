19c4aa4d797342694caef7ed403b5f075e724a4c
Sushant <105408469+sughics@users.noreply.github.com>
2023-01-18 15:13:58 +1100

    issue #175 adding edit role permission to licence actions to allow open access to licence index method via webservice

--- grails-app/controllers/au/org/ala/collectory/LicenceController.groovy
+++ grails-app/controllers/au/org/ala/collectory/LicenceController.groovy
@@ -111,42 +111,37 @@ class LicenceController {
     }
 
     def update(Long id, Long version) {
-        if (collectoryAuthService?.userInRole(grailsApplication.config.ROLE_ADMIN)) {
-            def licenceInstance = Licence.get(id)
-            if (!licenceInstance) {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'licence.label', default: 'Licence'), id])
-                redirect(action: "list")
-                return
-            }
+        def licenceInstance = Licence.get(id)
+        if (!licenceInstance) {
+            flash.message = message(code: 'default.not.found.message', args: [message(code: 'licence.label', default: 'Licence'), id])
+            redirect(action: "list")
+            return
+        }
 
-            if (version != null) {
-                if (licenceInstance.version > version) {
-                    licenceInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
-                            [message(code: 'licence.label', default: 'Licence')] as Object[],
-                            "Another user has updated this Licence while you were editing")
-                    render(view: "edit", model: [licenceInstance: licenceInstance])
-                    return
-                }
-            }
-
-            licenceInstance.properties = params
-
-            def savedInstance = null
-            Licence.withTransaction {
-                savedInstance = licenceInstance.save(flush: true)
-            }
-
-            if (!savedInstance) {
+        if (version != null) {
+            if (licenceInstance.version > version) {
+                licenceInstance.errors.rejectValue("version", "default.optimistic.locking.failure",
+                        [message(code: 'licence.label', default: 'Licence')] as Object[],
+                        "Another user has updated this Licence while you were editing")
                 render(view: "edit", model: [licenceInstance: licenceInstance])
                 return
             }
-
-            flash.message = message(code: 'default.updated.message', args: [message(code: 'licence.label', default: 'Licence'), licenceInstance.id])
-            redirect(action: "show", id: licenceInstance.id)
-        } else{
-            response.setHeader("Content-type", "text/plain; charset=UTF-8")
-            render(message(code: "provider.group.controller.04", default: "You are not authorised to access this page."))
         }
+
+        licenceInstance.properties = params
+
+        def savedInstance = null
+        Licence.withTransaction {
+            savedInstance = licenceInstance.save(flush: true)
+        }
+
+        if (!savedInstance) {
+            render(view: "edit", model: [licenceInstance: licenceInstance])
+            return
+        }
+
+        flash.message = message(code: 'default.updated.message', args: [message(code: 'licence.label', default: 'Licence'), licenceInstance.id])
+        redirect(action: "show", id: licenceInstance.id)
     }
 
     def delete(Long id) {
